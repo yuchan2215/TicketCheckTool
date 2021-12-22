9755918b8e0ae3c3d6972ce2f09fc9861b494e90
Miyayu <yuchan2215@gmail.com>
2021-12-22 23:37:49 +0900

    テストデータをランダムに表示するように変更

--- json-server/middleware.js
+++ json-server/middleware.js
@@ -4,8 +4,22 @@ function getRandomInt(max) {
 module.exports = (req,res,next) => {
     if(req.method === 'POST'){
         req.method = 'GET';
-        req.url =  '/api' + getRandomInt(4)
-        console.log("return :" + req.url)
+        const status = getRandomInt(4)
+        switch(status){
+            case 0:
+                console.log("売り切れ\t売り切れ")
+                break;
+            case 1:
+                console.log("販売中\t売り切れ")
+                break;
+            case 2:
+                console.log("売り切れ\t販売中")
+                break;
+            case 3:
+                console.log("販売中\t販売中")
+                break;
+        }
+        req.url =  '/api' + status
     }
     next()
 }
