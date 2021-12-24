d65fb221f917f6e92f4d1160f0f5d321b88c0599
Miyayu <yuchan2215@gmail.com>
2021-12-24 17:15:31 +0900

    エラー発生時にログを開業しないようにした

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ def getAPI():
     headerData = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36'}
     try:
         response = requests.post(arg0, params=requestData, headers=headerData)
-        print("データ取得完了",response.text[:30])
+        print("データ取得完了",response.text[:30].replace("\n",""))
         text = json.loads(response.text)
         return text
     except:
