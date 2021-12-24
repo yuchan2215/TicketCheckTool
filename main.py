95bee435eafa3108d73238a25364b30d91be76f9
Miyayu <yuchan2215@gmail.com>
2021-12-24 12:11:34 +0900

    データのプレビュー機能の追加

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ def run():
     requestData = {'useDays':1,"useDateFrom":arg1,'parkTicketSalesForm':1}
     headerData = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36'}
     response = requests.post(arg0, params=requestData, headers=headerData)
-    print("データ取得完了")
+    print("データ取得完了",response.text[:30])
 
     jsonData = json.loads(response.text)
     for i in jsonData:
